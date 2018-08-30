75cd90e45da6f88e29c8b415226783600d617a03
paulabarreto <paulavilaca@gmail.com>
2018-08-30 23:05:17 +0000

    total sales working

--- salesTax.js
+++ salesTax.js
@@ -21,3 +21,40 @@ var companySalesData = [
     sales: [ 500, 100 ]
   }
 ];
+
+
+function calculateSalesTax(salesData, taxRates) {
+  var sales = [];
+  var companies = {};
+  const reducer = (accumulator, currentValue) => accumulator + currentValue;
+
+  for(var i = 0; i < salesData.length; i++){
+    if(!companies[salesData[i].name]){
+      var s = (salesData[i].sales).reduce(reducer);
+      companies[salesData[i].name] = {totalSales: s, totalTaxes: 2};
+    } else {
+      var s2 = (salesData[i].sales).reduce(reducer);
+      companies[salesData[i].name].totalSales += s2;
+    }
+  }
+
+  return companies;
+  //var result = salesData.sales.reduce((a,b) => a + b);
+  //return result;
+}
+
+var results = calculateSalesTax(companySalesData, salesTaxRates);
+
+console.log(results);
+
+// Expected Results:
+// {
+//   Telus: {
+//     totalSales: 1300
+//     totalTaxes: 144
+//   },
+//   Bombardier: {
+//     totalSales: 800,
+//     totalTaxes: 40
+//   }
+// }
